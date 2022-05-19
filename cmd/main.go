72eee659b9b14d5f89fba75d631da0b9e997a48a
Klaus-Dnlk <79231530+Klaus-Dnlk@users.noreply.github.com>
2022-05-19 10:12:43 +0300

    fail handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	database "mssgs-crud/db"
+	"mssgs-crud/db"
 	"mssgs-crud/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	var db = database.Init()
-	r := gin.Default()
 
-	r.GET("/home", handlers.HomePage)
-	r.POST("/send", handlers.PostPage)
-	// r.PATCH("/", UpdatePage)
-	// r.DELETE("/del", DelPage)
-	r.Run()
+	db.Connection()
+
+	router := gin.Default()
+
+	router.GET("/home", handlers.GetUsers)
+	router.POST("/post", handlers.AddUser)
+
+	router.Run()
 }
